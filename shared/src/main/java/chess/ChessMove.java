35f6817f8f5807871f8e94788db0b7293183c1d4
IcYy17 <littlenash171@gmail.com>
2024-01-20 14:27:44 -0700

    phase 0 pt 9 ChessMove and Position Complete

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,7 +9,18 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.deepEquals(startPos, chessMove.startPos) && Objects.deepEquals(endPos, chessMove.endPos) && promotionPiece == chessMove.promotionPiece;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPos, endPos, promotionPiece);
+    }
 
     private ChessPosition startPos;
     private ChessPosition endPos;
