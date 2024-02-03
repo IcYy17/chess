9d07121d221af4abe3e534369beb304e15375116
IcYy17 <littlenash171@gmail.com>
2024-02-03 15:55:20 -0700

    phase 1 pt4 makeValidMoves

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,9 +1,7 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -13,18 +11,19 @@ import java.util.List;
  */
 public class ChessGame {
 
-    private ChessBoard board;
+    private ChessBoard chessBoard;
     private TeamColor turn;
+
     public ChessGame() {
-    board = new ChessBoard();
-    turn  = TeamColor.WHITE;
+        chessBoard = new ChessBoard();
+        turn = TeamColor.WHITE;
+        chessBoard.resetBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-
         return turn;
     }
 
@@ -34,7 +33,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-    turn = team;
+        turn = team;
     }
 
     /**
@@ -52,25 +51,21 @@ public class ChessGame {
      * @return Set of valid moves for requested piece, or null if no piece at
      * startPosition
      */
+
+    public Collection<ChessMove> getValidMoves(ChessPosition startPosition) {
+        ChessPiece piece = chessBoard.getPiece(startPosition);
+        if (piece != null) {
+            return piece.pieceMoves(chessBoard, startPosition);
+        }
+        return null;
+    }
+
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        ChessPiece piece = board.getPiece(startPosition);
-        if (piece == null) {
-            return Collections.emptyList();
-        }
-        
-        Collection<ChessMove> legalMoves = piece.pieceMoves(board, startPosition);
-        List<ChessMove> validMoves = new ArrayList<>();
+        return getValidMoves(startPosition);
+    }
 
-        for (ChessMove move : legalMoves) {
-            ChessBoard fakeBoard = board.clone();
-            fakeBoard.movePiece(move.getStartPosition(), move.getEndPosition());
-            
-            if (!isInCheck(piece.getTeamColor(), fakeBoard)) {
-                validMoves.add(move);
-            }
-        }
-
-        return validMoves;
+    private void switchTurn() {
+        turn = (turn == TeamColor.WHITE) ? TeamColor.BLACK : TeamColor.WHITE;
     }
 
     /**
@@ -80,7 +75,20 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = chessBoard.getPiece(move.getStartPosition());
+        if (piece != null && piece.getTeamColor() == turn) {
+            Collection<ChessMove> validMoves = getValidMoves(move.getStartPosition());
+
+            if (validMoves != null && validMoves.contains(move)) {
+                chessBoard.addPiece(move.getEndPosition(), piece);
+                chessBoard.addPiece(move.getStartPosition(), null);
+                switchTurn();
+            } else {
+                throw new InvalidMoveException("Invalid move");
+            }
+        } else {
+            throw new InvalidMoveException("Invalid move");
+        };
     }
 
     /**
@@ -120,7 +128,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        chessBoard = board;
     }
 
     /**
@@ -129,6 +137,19 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return chessBoard;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ChessGame chessGame = (ChessGame) object;
+        return Objects.deepEquals(chessBoard, chessGame.chessBoard) && turn == chessGame.turn;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chessBoard, turn);
     }
 }
