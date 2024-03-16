364a19b3548c627ba66ed19f700a328ea9d30d28
IcYy17 <littlenash171@gmail.com>
2024-03-15 18:07:26 -0600

    phase 5 fixed run bug

--- client/src/main/java/ui/ChessGameClient.java
+++ client/src/main/java/ui/ChessGameClient.java
@@ -8,7 +8,7 @@ import model.AuthInfo;
 public class ChessGameClient {
     private Server server;
     private String url;
-    private boolean serverRunning = true;
+    private boolean serverRunning;
     private Status status = Status.LoggedOut;
     public ChessGameClient() {
         server = new Server("http://localhost:1234");
@@ -38,44 +38,48 @@ public class ChessGameClient {
         };
 
     }
+    private void run(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE);
+        System.out.println(EscapeSequences.ERASE_SCREEN);
+        System.out.println(EscapeSequences.SET_TEXT_BOLD + "Welcome to CLI-Chess :)");
+        System.out.println(EscapeSequences.RESET_TEXT_BOLD_FAINT);
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_YELLOW);
+        System.out.println("Input 'help' for a list of commands or 'quit' to exit.");
+        serverRunning = true;
+        while(serverRunning){
+            System.out.print("$>");
+            var in = scan.nextLine();
+            var out = parser(in);
+
+            System.out.println(out);
+        }
+    }
     public String help() {
         StringBuilder helpMessage = new StringBuilder("Available commands:\n");
 
         if (status == Status.LoggedIn) {
-            helpMessage.append("- Create <name> - Create a new game\n")
-                    .append("- List - List all available games\n")
-                    .append("- Join <id> [white|black|<empty>] - Join a game\n")
+            helpMessage.append("- Create <name> - Create new game\n")
+                    .append("- List - List all games\n")
+                    .append("- Join <id> [white-black-<empty>] - Join game\n")
                     .append("- Observe <id> - Observe a game\n")
-                    .append("- Logout - Log out of the current account\n")
-                    .append("- Quit - Exit the program\n")
-                    .append("- Help - Display this message\n");
+                    .append("- Logout - Log out of account\n")
+                    .append("- Quit - Exit CLI-Chess\n")
+                    .append("- Help - Display help message\n");
         } else {
-            helpMessage.append("- Register <username> <password> <email> - Register a new account\n")
-                    .append("- Login <username> <password> - Log in to an existing account\n")
-                    .append("- Quit - Exit the program\n")
-                    .append("- Help - Display this message\n");
+            helpMessage.append("- Register <username> <password> <email> - Register new account\n")
+                    .append("- Login <username> <password> - Log in an account\n")
+                    .append("- Quit - Exit CLI-Chess\n")
+                    .append("- Help - Display help message\n");
         }
 
         return helpMessage.toString();
     }
 
-    private void run(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE);
-        System.out.println(EscapeSequences.ERASE_SCREEN);
-        System.out.println(EscapeSequences.SET_TEXT_BOLD + "Welcome to CLI-Chess :)" + EscapeSequences.RESET_TEXT_BOLD_FAINT);
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_YELLOW);
-        System.out.println("Input 'help' for a list of commands or 'quit' to exit.");
-        while(serverRunning){
-            var in = scan.nextLine();
-            var out = parser(in);
-            System.out.print("$>");
-            System.out.println(out);
-        }
-    }
+
     public String quit() {
         serverRunning = false;
-        return "Exiting CLI-Chess. Goodbye!";
+        return "Exiting CLI-Chess. Come back soon!";
     }
 
 }
