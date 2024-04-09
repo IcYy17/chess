1e39493f94963145d0409d7d4e8bda195217910e
IcYy17 <littlenash171@gmail.com>
2024-04-09 13:48:22 -0600

    phase 6 session edits

--- server/src/main/java/server/WebsocketSession.java
+++ server/src/main/java/server/WebsocketSession.java
@@ -1,25 +1,44 @@
 package server;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.eclipse.jetty.websocket.api.Session;
 public class WebsocketSession {
-    private Map<Integer, Map<String, Session>> sessionMap;
+    private Map<Integer, Map<String, Session>> sessions;
 
     public WebsocketSession() {
-        sessionMap = new HashMap<>();
+        sessions = new HashMap<>();
     }
 
     public void addSessionToGame(Integer gameID, String authToken, Session session) {
-
+        sessions.computeIfAbsent(gameID, k -> new HashMap<>()).put(authToken, session);
     }
 
     public void removeSessionFromGame(Integer gameID, String authToken, Session session) {
-
+        sessions.getOrDefault(gameID, Collections.emptyMap()).remove(authToken);
     }
     public void removeSession(Session session) {
+//        for (Map<String, Session> gameSessions : sessions.values()) {
+//
+//            Set<String> keysToRemove = gameSessions.entrySet().stream()
+//                    .filter(entry -> entry.getValue().equals(session))
+//                    .map(Map.Entry::getKey)
+//                    .collect(Collectors.toSet());
+//
+//
+//            for (String key : keysToRemove) {
+//                gameSessions.remove(key);
+//            }
+//        }
+        for (Map<String, Session> map : sessions.values()) {
+            map.values().remove(session);
+        }
 
     }
     public Map<String, Session> getSessionsForGame(Integer gameID) {
-        return null;
+        return sessions.get(gameID);
     }
 }
