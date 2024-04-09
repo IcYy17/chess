e9941c7a73b60e20385eb4161ac27b57ee4ce285
IcYy17 <littlenash171@gmail.com>
2024-04-08 22:18:36 -0600

    phase 6 more websocket

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -1,4 +1,49 @@
 package ui;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class WebSocketFacade {
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+import webSocketMessages.serverMessages.*; //need to add more here
+
+import webSocketMessages.userCommands.*; //need to add more here
+
+import chess.ChessGame;
+import chess.ChessMove;
+import exception.ResponseException;
+public class WebSocketFacade extends Endpoint {
+
+    private Session session;
+
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+    }
+
+    @OnClose
+    public void onClose(){
+    }
+
+    @OnError
+    public void onError(){
+    }
+
+
+
+
+
+
 }
--- server/src/main/java/server/WebsocketHandler.java
+++ server/src/main/java/server/WebsocketHandler.java
@@ -1,4 +1,38 @@
 package server;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+
+import chess.ChessGame.TeamColor;
+import exception.ResponseException;
+import model.GameInfo;
+
+import org.eclipse.jetty.websocket.api.*;
+
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import webSocketMessages.serverMessages.*; //need to add more to both server and user
+import webSocketMessages.userCommands.*;
+
+import webSocketMessages.userCommands.UserGameCommand;
+import webSocketMessages.serverMessages.ServerMessage;
+import service.*;
+import java.io.IOException;
+import java.util.Objects;
+
 public class WebsocketHandler {
+    private WebsocketSession sessions = new WebsocketSession();;
+    @OnWebSocketConnect
+    public void onConnect(Session session) throws IOException {
+        System.out.println("Connected");
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        System.out.println("Closed: " + statusCode + " " + reason);
+        sessions.removeSession(session);
+        session.close();
+    }
+
+
 }
